6a72618bad349528a11a42ebd8d9eceb23e77696
Zoratoo <gui_zorato@hotmail.com>
2024-03-26 08:39:40 -0300

    Adicionado pontos de embarque

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import TelaCadastroEscola from './telasCadastro/telaCadastroEscola.jsx';
 import TelaCadastroResponsavel from './telasCadastro/telaCadastroResponsavel.jsx';
 import TelaCadastroAluno from './telasCadastro/telaCadastroAluno.jsx';
+import TelaCadastroPontoEmbarque from './telasCadastro/telaCadastroPontoEmbarque.jsx';
 import Tela404 from './telasCadastro/Tela404.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TelaMenu from './telasCadastro/TelaMenu.jsx'
@@ -17,6 +18,7 @@ function App() {
             <Route path='/alunos' element={<TelaCadastroAluno />} />
             <Route path='/responsaveis' element={<TelaCadastroResponsavel />} />
             <Route path='/escolas' element={<TelaCadastroEscola />} />
+            <Route path='/pontos-embarque' element={<TelaCadastroPontoEmbarque />} />
             <Route path='/' element={<TelaMenu />} />
             <Route path='*' element={<Tela404 />} />
           </Routes>
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import escolaSlice from './escolaReducer.js';
 import responsavelSlice from './responsavelReducer.js';
 import alunoSlice from './alunoReducer.js';
+import pontosEmbarqueReducer from './pontosEmbarqueReducer.js';
 
 const store = configureStore({
     reducer: {
         escola: escolaSlice,
         responsavel: responsavelSlice,
-        aluno: alunoSlice
+        aluno: alunoSlice,
+        pontoEmbarque: pontosEmbarqueReducer
     }
 });
 
