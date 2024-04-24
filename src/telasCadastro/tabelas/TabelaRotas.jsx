bf8f62a12be10c7aef0655d639c1284bbd660dcd
JoaoValim <joaovitorvalimm@gmail.com>
2024-04-23 22:20:10 -0300

    atualizacao da rota

--- src/telasCadastro/tabelas/TabelaRotas.jsx
+++ src/telasCadastro/tabelas/TabelaRotas.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
-
+import { useState,useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { buscarRotas } from "../../redux/rotaReducer";
 
 export default function TabelaRotas(props){
-    
+    const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(buscarRotas());
+    }, [dispatch]);
+
+    const { estado, mensagem, rotas } = useSelector(state => state.rota);
+
+    console.log(rotas)
+
     return (
         <>
         
